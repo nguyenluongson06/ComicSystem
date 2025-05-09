7cda2fb5360af9301a42fc1f497482ef63423678
Nguyễn Duy Hưng <nguyenluongson06@gmail.com>
2025-05-09 20:01:32 +0700

    rental details

--- ComicSystem/Controllers/RentalDetailController.cs
+++ ComicSystem/Controllers/RentalDetailController.cs
@@ -9,5 +9,36 @@ namespace ComicSystem.Controllers;
 [ApiController] [Route("api/rentalDetail")]
 public class RentalDetailController(AppDbContext context) : ControllerBase
 {
+    [HttpPost] public async Task<ActionResult> Post([FromBody] RentalDetailDto dto)
+    {
+        try
+        {
+            var comicBook = await context.ComicBooks.FindAsync(dto.ComicBookId);
+            if (comicBook == null)
+            {
+                return Ok("Comic book not found");
+            }
 
+            var rental = await context.Rentals.FindAsync(dto.RentalId);
+            if (rental == null)
+            {
+                return Ok("Rental not found");
+            }
+
+            var rentalDetail = new RentalDetail
+            {
+                RentalId = dto.RentalId,
+                ComicBookId = comicBook.Id,
+                Quantity = dto.Quantity,
+                PricePerDay = comicBook.PricePerDay,
+            };
+            await context.RentalDetails.AddAsync(rentalDetail);
+            await context.SaveChangesAsync();
+            return Ok("Rental detail added");
+        }
+        catch (Exception ex)
+        {
+            return Ok("Error creating rental details:" + ex.Message);
+        }
+    }
 }
---
+++ ComicSystem/Dto/RentalDetailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ComicSystem.Dto;
+
+public class RentalDetailDto
+{
+    public int RentalId { get; set; }
+    public int ComicBookId { get; set; }
+    public int Quantity { get; set; }
+}
